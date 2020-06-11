37b6323af47efda08c5747e5cc3075ad1580e79c
lindsey tummond <lindseytummond@gmail.com>
2020-06-11 16:45:26 -0700

    adding image to article model

--- models/Article.js
+++ models/Article.js
@@ -9,30 +9,33 @@ var ArticleSchema = new Schema({
   // `title` is required and of type String
   title: {
     type: String,
-    required: true
+    required: true,
+    unique: { index: { unique: true } }
   },
-  // `summary` is required and of type String
-  summary: {
-    type: String,
-    required: true
-  },
-  // `link` is required and of type String
   link: {
     type: String,
-    required: true
+    required: false
+  },
+  summary: {
+    type: String,
+    required: false
+  },
+  image: {
+    type: String,
+    required: false
+  },
+  saved: {
+    type: Boolean,
+    required: true,
+    default:0
   },
   // `note` is an object that stores a Note id
   // The ref property links the ObjectId to the Note model
   // This allows us to populate the Article with an associated Note
-  note: {
+  note:[{
     type: Schema.Types.ObjectId,
     ref: "Note"
-  },
-  isSaved: {
-    type: Boolean,
-    default: false
-  }
-
+  }]
 });
 
 // This creates our model from the above schema, using mongoose's model method
